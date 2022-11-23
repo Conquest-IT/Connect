c292c66e80b9dffe938c35d079c1f83252bf4ade
Zahid <mrzahidxy@gmail.com>
2022-11-24 00:15:33 +0600

    ui: signup

--- src/pages/clientRoutes.jsx
+++ src/pages/clientRoutes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Signup from "./signup/Signup";
 import Login from "./login/Login";
-import Profile from "../components/profile/Profile.component";
+
 
 export default function clientRoutes() {
   return (
@@ -12,7 +12,7 @@ export default function clientRoutes() {
          
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/profile" component={Profile} />
+
         </Switch>
 
         {/* {true ? (
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,100 +1,125 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { MuiTelInput } from "mui-tel-input";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
 
 function Copyright(props) {
-    return (
-        <Typography variant="body2" color="text.secondary" align="center" {...props}>
-            {"Copyright © "}
-            <Link color="inherit" href="https://mui.com/">
-                Your Website
-            </Link>{" "}
-            {new Date().getFullYear()}
-            {"."}
-        </Typography>
-    );
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Connect.com
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
 const theme = createTheme();
 
 export default function SignUp() {
-    const history = useHistory();
-    const [phone, setPhone] = useState("+880");
+  const [user, setUser] = useState({});
 
-    const handlePhoneChange = event => {
-        let inPutNumberWithoutSpaces = event.target.value.split(" ").join("");
-        setPhone(inPutNumberWithoutSpaces);
-    };
+  const handleChange = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
 
-    const handleSubmit = event => {
-        event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-        console.log(phone);
-        history.push("/otp");
-    };
+    console.log(user);
+  };
 
-    return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign up
-                    </Typography>
-                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12}>
-                                <TextField required fullWidth label="Name" />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <MuiTelInput
-                                    label="Phone"
-                                    fullWidth
-                                    focusOnSelectCountry
-                                    preferredCountries={["BD"]}
-                                    onlyCountries={["BD"]}
-                                    value={phone}
-                                    onInput={handlePhoneChange}
-                                />
-                            </Grid>
-                        </Grid>
-                        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-                            Sign Up
-                        </Button>
-                        <Grid container justifyContent="flex-end">
-                            <Grid item>
-                                <Link href="#/login" variant="body2">
-                                    Already have an account? Sign in
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-                <Copyright sx={{ mt: 5 }} />
-            </Container>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+            <GroupAddIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  name="email"
+                  required
+                  fullWidth
+                  type="email"
+                  label="Email"
+                  value={setUser.email}
+                  onInput={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="password"
+                  required
+                  fullWidth
+                  type="password"
+                  label="Password"
+                  value={setUser.password}
+                  onInput={handleChange}
+                />
+                {/* <MuiTelInput
+                  label="Password"
+                  fullWidth
+                  focusOnSelectCountry
+                  preferredCountries={["BD"]}
+                  onlyCountries={["BD"]}
+                  value={phone}
+                  onInput={handlePhoneChange}
+                /> */}
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#/login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
 }
