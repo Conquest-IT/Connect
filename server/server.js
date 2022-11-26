7954a05a76ed8bb45eb3026aee3ff2ce47e39fd0
Mehedi Hassan <howlader.mehedihassan@gmail.com>
2022-11-26 22:16:20 +0600

    chore: add post and image route

--- server/server.js
+++ server/server.js
@@ -18,10 +18,14 @@ app.use(express.static(path.join(__dirname, "../dist")));
 // Implement Routes
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
+const imageRoutes = require("./routes/image.routes");
 
 // Use Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/image", imageRoutes);
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`);
